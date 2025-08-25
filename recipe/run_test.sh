2245bf322b66175c0b1df3b9793ce4636c9ff895
Min RK <benjaminrk@gmail.com>
2025-08-24 21:58:35 -0700

    build with stable ABI

--- recipe/run_test.sh
+++
@@ -1,8 +0,0 @@
-# aarch64 failing tests
-# /lib64/libm.so.6: version `GLIBC_2.27' not found
-if [[ "$target_platform" == linux-aarch64 && "$device" != host ]]; then
-  echo "skipping test"
-else
-  python -c "import petsc4py"
-  python -c "import petsc4py.PETSc"
-fi
